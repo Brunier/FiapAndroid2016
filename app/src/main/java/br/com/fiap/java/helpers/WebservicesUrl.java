fbbbb0548311b45ba55016a74e70f31c3d4e2e8f
Lucario-Reamp <lucario.reamp@gmail.com>
2016-10-07 22:33:53 -0300

    Atualizando a url

--- app/src/main/java/br/com/fiap/java/helpers/WebservicesUrl.java
+++ app/src/main/java/br/com/fiap/java/helpers/WebservicesUrl.java
@@ -4,13 +4,13 @@ package br.com.fiap.java.helpers;
  * Created by lucario on 9/28/16.
  */
 public enum WebservicesUrl {
-    Login("http://192.168.0.113:1225/login"),
-    Cadastrar("http://192.168.0.113:1225/cadastrar"),
-    Noticias("http://192.168.0.113:1225/noticias");
+    Login("http://13.65.98.255:1226/login"),
+    Cadastrar("http://13.65.98.255:1226/cadastrar"),
+    Noticias("http://13.65.98.255:1226/noticias");
 
     private String url;
     //private String IP = "192.168.0.113";
-    //private String IP = "10.0.2.2";
+    //private String IP = "10.0.2.2";a
 
     WebservicesUrl(String url) {
         this.url = url;
